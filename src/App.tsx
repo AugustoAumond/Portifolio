70dc596318d457d33bc52a81c6fd9865d7b5be15
Augusto Sturmer Aumond <80125507+AugustoAumond@users.noreply.github.com>
2025-03-25 18:33:57 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import Contact from './components/contact/contact';
 import Header from './components/header/header';
 import Projects from './components/projects/projects';
-import About from './components/about/about.tsx';
+import About from './components/about/about';
 
 
 
